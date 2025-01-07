b14d6bf856f7f71e0269a97d4f306dca2fc1a51e
Gallamion <rex_virtua@yahoo.com.br>
2025-01-07 09:40:00 -0300

    urls

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from recipe.views import home
+
+from . import views
+
+app_name = 'recipe'
 
 urlpatterns = [
-    path('', home)
+    path('', views.home, name="home"),
+    path('recipe/<int:id>/', views.recipe, name="recipe")
 ]
