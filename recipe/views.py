60bbd9c0792e32730fe0ff659386232c3063988e
Gallamion <rex_virtua@yahoo.com.br>
2025-01-12 18:04:32 -0300

    ajustando nomes

--- recipe/views.py
+++ recipe/views.py
@@ -1,25 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.recipe.factory import make_recipe
 from recipe.models import Recipe
 
 
 def home(request):
-    recipe = Recipe.objects.all().order_by('-id')
+    recipe = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipe/pages/home.html', context={
         'recipes': recipe,
     })
 
 
-def category(request, category_id):
-    recipe = Recipe.objects.filter(category__id=category_id).order_by('-id')
-    return render(request, 'recipe/pages/home.html', context={
+def category(request, category_id):    
+    recipe = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True
+        ).order_by('-id'))
+
+    return render(request, 'recipe/pages/category.html', context={
         'recipes': recipe,
+        'title': f'{recipe[0].category.name} - Category | '
     })
 
 
 def recipe(request, id):
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
+
     return render(request, 'recipe/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
 
